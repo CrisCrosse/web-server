1c36332c238a6af560627ede295869e82b0c5e7a
Chris Rossell <chris.rossell@autotrader.co.uk>
2024-07-04 18:39:56 +0100

    make server accept 10 requests then close, add better error handling, separate accept connection and handling request

--- src/main/java/webServer.java
+++ src/main/java/webServer.java
@@ -1,32 +1,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.TimeUnit;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 public class webServer extends ServerSocket {
+    // this class is responsible for accepting connections from clients, and passing connection details to ServerIO
+    // ? it will record how many successful requests it has handled and print this to the console after it shuts?
         private Socket client;
         final private int port;
+        private int requestCount = 0;
 
     public webServer(int port) throws IOException {
         super(port);
         this.port = port;
     }
 
-    public void run(){
-        try {
-            this.acceptConnection();
-            client.close();
-        } catch (Exception e){
-            System.out.printf("An error occurred accepting a connection to the web server on port %d.%n", this.port);
-            System.out.println(e);
-            System.out.println("Closing connection ...");
+    public void acceptTenRequests(){
+        // implement multi-threading to handle multiple requests at once and keep the server running until I send a close command
+
+        while (requestCount < 10){
+            try {
+                client = this.acceptConnection();
+            } catch (Exception e){
+                // this block gets hit no matter which part of the code fails --> so this error message is not too helpful
+                System.out.printf("An error occurred accepting a connection to the web server on port %d.%n", this.port);
+                System.out.println(e);
+            }
+                // handle request? one function
+            try {
+                this.handleRequest(client);
+            } catch (Exception e){
+                System.out.println("An error occurred handling the request.");
+                System.out.println(e);
+            }
+            try {
+                client.close();
+            } catch (Exception e){
+                System.out.println("An error occurred closing the connection.");
+                System.out.println(e);
+            }
+            requestCount++;
         }
     }
 
-    private void acceptConnection() throws IOException, InterruptedException {
+    private Socket acceptConnection() throws IOException, InterruptedException {
             System.out.println("Listening for connection on port 80....");
             System.out.println();
 
@@ -36,17 +56,22 @@ public class webServer extends ServerSocket {
             System.out.println();
             TimeUnit.SECONDS.sleep(1);
 
-            BufferedReader takeInputFromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            PrintWriter writeOutToClient = new PrintWriter(client.getOutputStream(), true);
-        ServerIO serverIO = new ServerIO(takeInputFromClient, writeOutToClient);
+            return client;
+    }
 
-            serverIO.respondToConnection();
+    private void handleRequest(Socket client) throws IOException {
+        // this function will handle the request from the client by instantiating a ServerIO object and calling the respondToConnection method
+        BufferedReader takeInputFromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        PrintWriter writeOutToClient = new PrintWriter(client.getOutputStream(), true);
+
+        ServerIO serverIO = new ServerIO(takeInputFromClient, writeOutToClient);
+        serverIO.respondToConnection();
     }
 
     public static void main(String[] args) throws IOException {
 
         webServer myServer = new webServer(80);
-        myServer.run();
+        myServer.acceptTenRequests();
         myServer.close();
 
 
